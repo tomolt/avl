e3506791be0bbb5bb531376ed76b4ec06047390c
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2021-02-27 14:31:25 +0100

    Insertion works now.

--- avl.c
+++ avl.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAXDEPTH 64
 
@@ -24,6 +25,8 @@ struct Node {
 	Edge      edges[2];
 };
 
+static AVL Tree;
+
 static void
 rotate(Edge *e, int d)
 {
@@ -37,6 +40,8 @@ rotate(Edge *e, int d)
 	b = NODE(*f);
 	y = BALAN(*f);
 	g = &b->edges[!d];
+	
+	printf("ROT [%03ju] %s\n", a->key, d ? "CCW" : "CW");
 
 	if (d) { /* CCW rotation */
 		x = x - 1 - MAX(y, 0);
@@ -125,13 +130,35 @@ avl_insert(AVL *avl, uintmax_t key, void *value)
 		if (balan < -1 || balan > 1) {
 			balance(edge);
 		}
+		balan = BALAN(*edge);
 		if (!balan) break;
 	}
 	
 	return 0;
 }
 
-#include <stdio.h>
+static int
+check_edge(Edge edge)
+{
+	Node *node;
+	int bal, l, h;
+	if (!edge) return 0;
+	node = NODE(edge);
+	bal  = BALAN(edge);
+	l = check_edge(node->edges[0]);
+	if (l < 0) return l;
+	h = check_edge(node->edges[1]);
+	if (h < 0) return h;
+	if (bal != h - l) return -1;
+	if (bal < -1 || bal > 1) return -2;
+	return MAX(l, h) + 1;
+}
+
+int
+avl_check(AVL avl)
+{
+	return check_edge((Edge) avl);
+}
 
 static void
 graph_rec(Edge edge, int col, FILE *file)
@@ -168,13 +195,21 @@ avl_graph(AVL avl, FILE *file)
 int
 main()
 {
-	AVL avl = NULL;
+	uintmax_t key;
 	int i;
+
+	Tree = NULL;
 	srand(0);
 	for (i = 0; i < 100; i++) {
-		avl_insert(&avl, rand() % 1000, NULL);
+		key = rand() % 1000;
+		printf("#%d INSERT [%03ju]\n", i, key);
+		avl_insert(&Tree, key, NULL);
+		avl_graph(Tree, stdout);
+		if (avl_check(Tree) < 0) {
+			printf("INCONSISTENCY\n");
+			break;
+		}
 	}
-	avl_graph(avl, stdout);
 	return 0;
 }
 
